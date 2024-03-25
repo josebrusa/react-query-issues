aefb64a1c1e901d9e81a7d2431856ffbd765a0b0
jose brusa <josebrusa@outlook.com>
2024-03-25 17:16:02 +0100

    finish de section 3

---
+++ src/api/gitHubApi.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const gitHubApi = axios.create({
+    baseURL: "https://api.github.com/repos/facebook/react",
+    headers: {
+
+    }
+}); 
---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import { Label } from "../interfaces/label";
+import { gitHubApi } from "../../api/gitHubApi";
+import { sleep } from "../../helpers/sleep";
+
+
+const getLabels = async (): Promise<Label[]> => {
+
+    await sleep(2)
+    const { data } = await gitHubApi.get('/labels');
+    console.log(data);
+
+    return data
+}
+
+export const useLabels = () => {
+    const labelsQuery = useQuery(
+        [ 'labels' ],
+        getLabels,
+        {
+            //staleTime: 1000 * 60 * 60,
+            //placeholderData:[],
+            initialData: [
+
+                {
+                    id: 2281766624,
+                    node_id: "MDU6TGFiZWwyMjgxNzY2NjI0",
+                    url: "https://api.github.com/repos/facebook/react/labels/Component:%20Scheduling%20Profiler",
+                    name: "Component: Scheduling Profiler",
+                    color: "1dc3d6",
+                    default: false,
+                },
+                {
+                    id: 204945357,
+                    node_id: "MDU6TGFiZWwyMDQ5NDUzNTc=",
+                    url: "https://api.github.com/repos/facebook/react/labels/Component:%20Shallow%20Renderer",
+                    name: "Component: Shallow Renderer",
+                    color: "eb6420",
+                    default: false,
+                }
+            ]
+        }
+    )
+
+    return labelsQuery
+}
