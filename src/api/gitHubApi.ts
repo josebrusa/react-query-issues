97185954a20d0eb22b407db882412a5154dea4f4
jose brusa <josebrusa@outlook.com>
2024-03-25 23:34:04 +0100

    fin de la seccion 4

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const gitHubApi = axios.create({
     baseURL: "https://api.github.com/repos/facebook/react",
     headers: {
-        Authorization: 'Bearer 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'
+        // Authorization: 'Bearer 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'
     }
 }); 
