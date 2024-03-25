f315a7a2d459e650c1cfa06d92ff9ab7b853fc9c
jose brusa <josebrusa@outlook.com>
2024-03-25 23:56:34 +0100

    finish de section 4 sin tokken

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -3,5 +3,6 @@ import axios from "axios";
 export const gitHubApi = axios.create({
     baseURL: "https://api.github.com/repos/facebook/react",
     headers: {
+        Authorization: 'Bearer '
     }
 }); 
